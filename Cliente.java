6039426ac8eea3d4b606f85e0e2b70605fb0e68f
Alisson1471 <alissonalvesdossantosoliveira@gmail.com>
2024-03-13 11:59:23 -0300

    Commitando nova linha de código

--- Cliente.java
+++ Cliente.java
@@ -1,7 +1,3 @@
 public class Cliente {
-    private String tipoConta;
-    private String nome;
-    private String cpf;
-    private int agencia;
-    private double saldo;
+    Cliente cliente1 = new Cliente();
 }
